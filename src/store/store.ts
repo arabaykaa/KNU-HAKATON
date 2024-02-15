2da3b681b79f9f9b756542b2d1a0a25e8906bbd1
Арапбай Абдужапаров <aabdujaparov@it.gov.kg>
2024-02-15 12:44:35 +0600

    adde useAppDispatch, useAppSelectro, modified store.ts

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,5 +1,10 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+
+const combireReducers = combineReducers({});
 
 export const store = configureStore({
-  reducer: {},
+  reducer: combireReducers,
 });
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
